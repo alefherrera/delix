73aaa6955f6876b45a19b620fafaa9bd56d6583e
alefherrera <=>
2016-09-22 00:17:53 -0300

    hot reload activado

--- client/src/app.js
+++ client/src/app.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './config';
-
 import { AppContainer } from 'react-hot-loader';
+
 const rootEl = document.getElementById('root');
 ReactDOM.render(
   <AppContainer>
     <App />
   </AppContainer>,
-  rootEl
-);
+   rootEl
+ );
 
 if (module.hot) {
   module.hot.accept('./config', () => {
+    // If you use Webpack 2 in ES modules mode, you can
+    // use <App /> here rather than require() a <NextApp />.
     const NextApp = require('./config').default;
     ReactDOM.render(
       <AppContainer>
         <NextApp />
       </AppContainer>,
       rootEl
-    )
-  })
+    );
+  });
 }
--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Title from './components/Title';
 
 const App = () => (
   <div>
-    App
+    <Title />
   </div>
 );
 
