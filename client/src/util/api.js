b2a317fe9b0b0a8dabc0347321faf92be0667b07
alefherrera <=>
2016-09-25 21:38:58 -0300

    fix para enviar post desde webclient

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -2,7 +2,16 @@ const HOST = 'http://localhost:3001';
 const getApi = endpoint => `${HOST}/${endpoint}`;
 const API = {
   get: url => fetch(getApi(url)).then(r => r.json()),
-  post: (url, obj = {}) => fetch(getApi(url), obj).then(r => r.json()),
+  post: (url, obj = {}) => fetch(getApi(url),
+    {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(obj),
+    }
+  ).then(r => r.json()),
 };
 
 export default {
