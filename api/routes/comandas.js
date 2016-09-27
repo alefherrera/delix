7b8c3a8d5886d516cdf6221d164d7bb7a7e6b915
alefherrera <=>
2016-09-26 23:42:24 -0300

    generacion de comandas con productos

--- api/routes/comandas.js
+++ api/routes/comandas.js
@@ -3,11 +3,12 @@ module.exports = router => {
   router.post('/comandas', (req, res) => {
     const comanda = req.body;
     Models.comandas.create(comanda).then(r => {
+      debugger;
       if (comanda.productos_id) {
         return Models.productosPorComandas.create({
           comandas_id: r.id,
           productos_id: comanda.productos_id,
-        }).then(r => res.json());
+        }).then(r => res.json(r));
       }
     });
   });
--- api/routes/index.js
+++ api/routes/index.js
@@ -8,4 +8,5 @@ module.exports = (router, io) => {
   require('./proveedores')(router);
   require('./sectores')(router);
   require('./pedidos')(router);
+  require('./comandas')(router);
 };
