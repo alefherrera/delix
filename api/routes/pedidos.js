ecf136974fdcaa17dc981bb7a4ecdf8dcf2a9fbc
Andres Blas Pujadas <andiblas@gmail.com>
2016-10-01 21:18:15 -0300

    adicion de comandas al recuperar un pedido

--- api/routes/pedidos.js
+++ api/routes/pedidos.js
@@ -5,15 +5,29 @@ module.exports = router => {
     //util.abml(router, 'pedidos');
 
     router.post('/pedidos', (req, res) => {
-        const param = {where: req.body, include: [
-            {
-                model: Models.usuarios
-            }, {
-                model: Models.grupoDeMesas
-            }, {
-                model: Models.pedidoEstado
-            }
-        ]};
+        const param = {
+            where: req.body,
+            include: [
+                {
+                    model: Models.usuarios
+                }, {
+                    model: Models.grupoDeMesas
+                }, {
+                    model: Models.pedidoEstado
+                }, {
+                    model: Models.comandas,
+                    include: [
+                        {
+                            model: Models.productosPorComandas
+                        }, {
+                            model: Models.platosPorComandas
+                        }, {
+                            model: Models.promosPorComandas
+                        }
+                    ]
+                }
+            ]
+        };
 
         Models.pedidos.findOrCreate(param).then(result => res.json(result[0]));
 
