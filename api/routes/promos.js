68bc77c3d260adb0e1a44c1cbf28440b7c7a1c15
Andres Blas Pujadas <andiblas@gmail.com>
2016-10-07 00:21:47 -0300

    correcciones en promos.findAll()

--- api/routes/promos.js
+++ api/routes/promos.js
@@ -4,61 +4,56 @@ const util = require('./util');
 module.exports = router => {
     util.b(router, 'promos');
 
-router.post('/promos', (req, res) => {
-    const promoToSave = req.body;
-    console.log(promoToSave);
+    router.post('/promos', (req, res) => {
+        const promoToSave = req.body;
+        console.log(promoToSave);
 
-    if (!promoToSave.id) {
-      Models.promos.create(promoToSave).then(result => {
-        const promo = result;
-        Promise.all(
-          promo.setPlatos(promoToSave.platos.map((plato) => plato.id)),
-          promo.setProductos(promoToSave.productos.map((producto) => producto.id))
-        ).then(res.json(promo));
-      });
-    }
-    else
-    {
-      Models.promos.upsert(promoToSave).then(() => {
-        Models.promos.findById(promoToSave.id).then(promo =>{
-          Promise.all(
-            promo.setPlatos(promoToSave.platos.map((plato) => plato.id)),
-            promo.setProductos(promoToSave.productos.map((producto) => producto.id))
-          ).then(res.json(promo));
-        });
-      });
-    }
-});
+        if (!promoToSave.id) {
+            Models.promos.create(promoToSave).then(result => {
+                const promo = result;
+                Promise.all(promo.setPlatos(
+                  promoToSave.platos.map((plato) => plato.id)),
+                  promo.setProductos(promoToSave.productos.map((producto) => producto.id))
+                ).then(res.json(promo));
+            });
+        } else {
+            Models.promos.upsert(promoToSave).then(() => {
+                Models.promos.findById(promoToSave.id).then(promo => {
+                    Promise.all(
+                      promo.setPlatos(promoToSave.platos.map((plato) => plato.id)),
+                      promo.setProductos(promoToSave.productos.map((producto) => producto.id))
+                    ).then(res.json(promo));
+                });
+            });
+        }
+    });
 
     router.get('/promos', (req, res) => {
-
         Models.promos.findAll({
+          include: [
+              {
+                  model: Models.platos
+              }, {
+                  model: Models.productos
+              }
+          ]
+        }).then(result => {
+            res.json(result);
+        });
+    });
+
+    router.get('/promos/:id', (req, res) => {
+        Models.promos.findById(req.params.id, {
             include: [
                 {
-                    model: Models.productosPorPromos,
-                    include: [Models.productos]
+                    model: Models.platos
                 }, {
-                    model: Models.platosPorPromos,
-                    include: [Models.platos]
+                    model: Models.productos
                 }
             ]
         }).then(result => {
             res.json(result);
         });
     });
-
-router.get('/promos/:id', (req, res) => {
-    Models.promos.findById(req.params.id, {
-        include: [
-            {
-                model: Models.platos
-            }, {
-                model: Models.productos
-            }
-        ]
-    }).then(result => {
-        res.json(result);
-    });
-});
 
 };
