20954e651e7df5c4cef3f55c3e295ae2f2812feb
Alejandro <alef.herrera@gmail.com>
2016-10-18 15:26:27 -0300

    api funcionando en android

--- client/webpack.config.android.js
+++ client/webpack.config.android.js
@@ -12,7 +12,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, 'dist/android'),
+    path: path.resolve(__dirname, '../android/delix/app/src/main/assets'),
     publicPath: '/static/',
     filename: 'bundle.js',
   },
