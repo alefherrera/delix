3f7a3a4fb7fd633f9f303c291118cffbba1d93bf
Andres Blas Pujadas <andiblas@gmail.com>
2016-11-01 16:57:12 -0300

    generacion de PDF para imprimir el ticket

--- api/routes/pedidos.js
+++ api/routes/pedidos.js
@@ -1,6 +1,7 @@
 const util = require('./util');
 const {Models} = require('../db');
 const _ = require('lodash');
+const jsreport = require('jsreport');
 
 const searchParam = () => (
   {
@@ -178,5 +179,52 @@ module.exports = (router, io) => {
         });
     });
 
+
+    router.get('/generar_ticket/:id', (req, res) => {
+        const fs = require('fs');
+
+        let param = searchParam();
+        param.where = {
+            pedidoEstadoId: 2,
+            id: req.params.id
+        };
+
+        Models.pedidos.findOne(param).then(pedido => {
+            if (!pedido) {
+                res.status(404).send();
+            }
+
+            const order = summarizeOrder(pedido);
+            let datosString = '';
+            let comandasString = '';
+            let pagoString = '';
+
+            datosString += `<tr><td>Mozo</td><td>${order.usuario.nombre + ' ' +  order.usuario.apellido}</td></tr>`;
+            datosString += `<tr><td>Mesa</td><td>${order.grupoDeMesas.nombre}</td></tr>`;
+            datosString += `<tr><td>Recepcion</td><td>${order.createdAt}</td></tr>`;
+            datosString += `<tr><td>Cierre</td><td>${order.modifiedAt || ''}</td></tr>`;
+
+            comandasString = '<tr><td>Detalle</td><td>Cantidad</td><td>Precio unit.</td><td>Total</td></tr>';
+            comandasString += _.reduce(order.orderLines, (prev, current) =>
+            {
+              return prev += `<tr><td>${current.descripcion}</td><td>${current.cantidad}</td><td>${current.precio}</td><td>${current.precioTotal}</td></tr>`;
+            }, '');
+
+            pagoString += `<tr><td>Total</td><td>${order.total}</td></tr>`;
+
+            let template = fs.readFileSync('./htmlTemplate/index.html').toString();
+            const result = template
+              .replace(/__tbl_Datos__/g, datosString)
+              .replace(/__tbl_comandas__/g, comandasString)
+              .replace(/__tbl_pagos__/g, pagoString);
+
+            jsreport.render(result).then(function(out) {
+              out.stream.pipe(res);
+            }).catch(function(e) {
+              res.end(e.message);
+            });
+
+        });
+    });
 
 };
