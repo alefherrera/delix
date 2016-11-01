8c091e929af2ccd43e0a54c3bf063188dd4e77d0
Alejandro <alef.herrera@gmail.com>
2016-11-01 17:51:10 -0300

    mostrar ticket de pago

--- client/src/components/Payment/PaymentRow.js
+++ client/src/components/Payment/PaymentRow.js
@@ -1,19 +1,22 @@
 import React, { PropTypes } from 'react';
 import { Card, CardText } from 'material-ui/Card';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Link } from 'react-router';
 
 const PaymentRow = ({ payment }) => (
   <Card>
     <CardText>
       <Grid>
-        <Row>
-          <Col xs>
-            {payment.pagoTipo.descripcion}
-          </Col>
-          <Col xs>
-            {`$${payment.montoRecibido}`}
-          </Col>
-        </Row>
+        <Link to={`/generar_ticket_pago/${payment.id}`} target="_blank">
+          <Row>
+            <Col xs>
+              {payment.pagoTipo.descripcion}
+            </Col>
+            <Col xs>
+              {`$${payment.montoRecibido}`}
+            </Col>
+          </Row>
+        </Link>
       </Grid>
     </CardText>
   </Card>
