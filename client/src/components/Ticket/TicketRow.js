48d4782e19ca8886a697f968036ca9bf1da9bb4f
Alejandro <alef.herrera@gmail.com>
2016-11-01 15:31:51 -0300

    no message

--- client/src/components/Ticket/TicketRow.js
+++ client/src/components/Ticket/TicketRow.js
@@ -4,62 +4,55 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import Divider from 'material-ui/Divider';
 import styles from './style.css';
 
-class TicketRow extends React.Component {
-
-  render() {
-    const { ticket } = this.props;
-    return (
-      <Card className={styles.item}>
-        <CardTitle
-          title={ticket.grupoDeMesas.nombre}
-          subtitle={`${ticket.usuario.nombre} ${ticket.usuario.apellido}`}
-        />
-        <Grid fluid>
-          <Row className={styles.row}>
+const TicketRow = ({ ticket }) => (
+  <Card className={styles.item}>
+    <CardTitle
+      title={ticket.grupoDeMesas.nombre}
+      subtitle={`${ticket.usuario.nombre} ${ticket.usuario.apellido}`}
+    />
+    <Grid fluid>
+      <Row className={styles.row}>
+        <Col xs={6}>
+          Descripcion
+        </Col>
+        <Col xs={2}>
+          #
+        </Col>
+        <Col xs={2}>
+          Precio
+        </Col>
+        <Col xs={2}>
+          Total
+        </Col>
+      </Row>
+      <Divider />
+      {
+        ticket.orderLines.map((orderLine, i) => (
+          <Row key={i} className={styles.row}>
             <Col xs={6}>
-              Descripcion
+              {orderLine.descripcion}
             </Col>
             <Col xs={2}>
-              #
+              {orderLine.cantidad}
             </Col>
             <Col xs={2}>
-              Precio
+              {`$${orderLine.precio}`}
             </Col>
             <Col xs={2}>
-              Total
+              {`$${orderLine.precioTotal}`}
             </Col>
           </Row>
-          <Divider />
-          {
-            ticket.orderLines.map((orderLine, i) => (
-              <Row key={i} className={styles.row}>
-                <Col xs={6}>
-                  {orderLine.descripcion}
-                </Col>
-                <Col xs={2}>
-                  {orderLine.cantidad}
-                </Col>
-                <Col xs={2}>
-                  {`$${orderLine.precio}`}
-                </Col>
-                <Col xs={2}>
-                  {`$${orderLine.precioTotal}`}
-                </Col>
-              </Row>
-            ))
-          }
-          <Divider />
-          <Row center="xs">
-            <Col xs>
-              <CardTitle title={`$${ticket.total}`} />
-            </Col>
-          </Row>
-        </Grid>
-      </Card>
-    );
-  }
-
-}
+        ))
+      }
+      <Divider />
+      <Row center="xs">
+        <Col xs>
+          <CardTitle title={`$${ticket.total}`} />
+        </Col>
+      </Row>
+    </Grid>
+  </Card>
+);
 
 TicketRow.propTypes = {
   ticket: PropTypes.object,
